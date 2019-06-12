49dffc85785ed75b5dacf14b160b996b6502277c
kyahn23 <kyahn23@gmail.com>
2019-06-12 14:30:05 +0900

    190612 14:29

--- resources/static/resources/js/index.js
+++ resources/static/resources/js/index.js
@@ -1,20 +1,60 @@
-alert('자바스크립트 연결 성공!!');
+var app = (function () {
+    let init = function () {
+        login_form()
+    }
 
-var wrapper = document.querySelector('#wrapper');
-wrapper.innerHTML = '<h1>SPA 시작</h1>'
-+'<div id="del">삭제할 내용</div>'
-+'<button id="btn">지워버리자</button>'
-+'<button id="btn2">추가하자</button>';
+    let login_form = function () {
+
+        let wrapper = document.querySelector('#wrapper');
+        wrapper.innerHTML = '<form action="/action_page.php">'
+            + '  아이디:<br>'
+            + '  <input type="text" name="loginid">'
+            + '  <br>'
+            + '  비밀번호:<br>'
+            + '  <input type="password" name="loginpasswd">'
+            + '  <br><br>'
+            + '  <input type="button" value="로그인">'
+            + '  <input id="sign" type="button" value="회원가입">'
+            + '</form>';
 
 
-var btn = document.querySelector('#btn');
-var btn2 = document.querySelector('#btn2');
-// var temp = document.querySelector('#del');
-btn.addEventListener('click',function(){
-    document.querySelector('#del').innerHTML = "";
-    // temp.innerHTML = "";
-});
-btn2.addEventListener('click',function(){
-    var addtext = document.createTextNode('새로추가됨');
-    document.querySelector('#del').appendChild(addtext);
-})
+        let btn = document.querySelector('#sign');
+
+        btn.addEventListener('click', function () {
+            signup_form();
+        });
+    }
+
+    let signup_form = function () {
+        let wrapper = document.querySelector('#wrapper');
+
+        wrapper.innerHTML = '<form action="/action_page.php">'
+            + '  아이디:<br>'
+            + '  <input type="text" name="userid">'
+            + '  <br>'
+            + '  비밀번호:<br>'
+            + '  <input type="password" name="userpasswd">'
+            + '  <br>'
+            + '  이름:<br>'
+            + '  <input type="text" name="username">'
+            + '  <br>'
+            + '  생년월일:<br>'
+            + '  <input type="date" name="bdate">'
+            + '  <br>'
+            + '  전화번호:<br>'
+            + '  <input type="tel" name="phone">'
+            + '  <br><br>'
+            + '  <input id="signup" type="button" value="회원가입">'
+            + '  <input type="reset" value="취소">'
+            + '</form>';
+
+        let btn2 = document.querySelector("#signup");
+        btn2.addEventListener('click', function () {
+            login_form();
+        });
+    }
+
+    return {
+        init: init
+    };
+})();
